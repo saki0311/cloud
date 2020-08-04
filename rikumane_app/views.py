b7a13848498a93a15b2e451e57c1ad723cc1f7d5
gen <sono2510@gmail.com>
2020-08-04 15:57:10 +0900

    index表示

--- rikumane_app/views.py
+++ rikumane_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rikumane_app import data
+
 
 # Create your views here.
+def index(request):
+    return render(request,'rikumane_app/index.html',{'data.company_data':data.company_data})
