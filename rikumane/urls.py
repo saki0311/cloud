8ed6ea254dae4991c890c2ad0413ba747850b9e2
gen <sono2510@gmail.com>
2020-08-08 23:52:35 +0900

    ページ遷移

--- rikumane/urls.py
+++ rikumane/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rikumane_app.urls')),
+    path('', include('rikumane_app.urls', 'rikumane_app')),
 ]
--- rikumane_app/views.py
+++ rikumane_app/views.py
@@ -4,4 +4,12 @@ from rikumane_app import data
 
 # Create your views here.
 def index(request):
-    return render(request,'rikumane_app/index.html',{'data.company_data[0]':data.company_data})
+    params = {
+        'Company0': data.company_data[0]['Company'],
+        'URL0': data.company_data[0]['URL'],
+        'ID0': data.company_data[0]['id'],
+    }
+    return render(request,'rikumane_app/index.html', params)
+
+def ivents(request):
+    return render(request,'rikumane_app/ivents.html')
