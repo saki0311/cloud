9240a8b1d0cda7bc69f76f72059593c2c5bb19ad
gen <sono2510@gmail.com>
2020-08-08 21:43:28 +0900

    index表示

--- rikumane_app/views.py
+++ rikumane_app/views.py
@@ -4,4 +4,4 @@ from rikumane_app import data
 
 # Create your views here.
 def index(request):
-    return render(request,'rikumane_app/index.html',{'data.company_data':data.company_data})
+    return render(request,'rikumane_app/index.html',{'data.company_data[0]':data.company_data})
