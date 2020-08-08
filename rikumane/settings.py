514fb35c776edad01ec36762e63f401bd6ed45a8
Kodai Murakami <k31m03.1020@gmail.com>
2020-08-08 15:34:20 +0900

    修正

--- rikumane/settings.py
+++ rikumane/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rikumane_app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'rikumane.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -117,4 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
+'''
+STATICFIES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+    BASE_DIR,
+]
+STATIC_URL = '/static/'
+'''
--- rikumane_app/views.py
+++ rikumane_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rikumane_app import calendar
+from .data import company_data
 
-# Create your views here.
+def index(request):
+    elem = company_data[0]
+    d = {
+        "Company":elem['Company'],
+        "URL":elem['URL'],
+        "id":elem['id'],
+        "Events":elem['Events'][0]['イベント名']
+    }
+    return render(request,'index.html',d)
