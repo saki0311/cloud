2c03fe917f509e4efc43030a93d5402be5c03133
Kodai Murakami <k31m03.1020@gmail.com>
2020-08-09 16:56:44 +0900

    ページ修正しました

--- rikumane_app/urls.py
+++ rikumane_app/urls.py
@@ -6,5 +6,4 @@ app_name='rikumane_app'
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('ivents/', views.ivents, name='ivents'),
-    path('add_event/',views.add_event,name='add_event'),
 ]
