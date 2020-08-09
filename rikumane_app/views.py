c28e8625da59fc032f2e5dc7229ce99e4ea8fc84
Kodai Murakami <k31m03.1020@gmail.com>
2020-08-10 03:48:26 +0900

    ページと企業修正版

--- rikumane_app/views.py
+++ rikumane_app/views.py
@@ -2,17 +2,30 @@ from django.shortcuts import render
 from rikumane_app import calendar
 from .data import company_data
 
+'''
+index用関数　企業データを全てindexに返す
+'''
 def index(request):
     params = {
         'data':company_data,
     }
     return render(request,'index.html', params)
 
+''' 
+・detail用関数　
+選択したクエリパラメータと同じidをもつ企業だけを抽出して
+detail.htmlへ返す
+
+・変数名　
+comapny -> 選択中の企業の辞書
+data -> 全ての企業データ
+
+'''
 def detail(request):
     con = int(request.GET.get('id'))
     for one in company_data:
         if one['unique_id'] == con:
-            res = one['info']
+            res = one
             break
     params = {
         'company':res,
@@ -21,12 +34,31 @@ def detail(request):
     return render(request,'detail.html', params)
 
 
+''' 
+・ivents用関数　
+- 選択したクエリパラメータと同じidをもつ企業だけを抽出して
+  detail.htmlへ返す
+- 入力したカレンダーの予定をGoogleAPIと連携して自分のカレンダーへ登録
+
+・変数名　
+comapny -> 選択中の企業の辞書
+data -> 全ての企業データ
+title -> 予定名
+start_time -> 予定開始時刻
+end_time -> 予定終了時刻
+'''
 def ivents(request):
+    con = int(request.GET.get('id'))
+    for one in company_data:
+        if one['unique_id'] == con:
+            res = one
+            break
     d={
     'data':company_data,
     'title':request.GET.get('name'),
     'start_time':request.GET.get('start_time'),
     'end_time':request.GET.get('end_time'),
+    'company':res,
     }
     if request.GET.get('name') and request.GET.get('start_time') and request.GET.get('end_time'):
         d['start_time'] += ":00"
--- static/js/user_define.js
+++ static/js/user_define.js
@@ -7,7 +7,7 @@ function copyToClipboard(){
       // テキストエリアオブジェクトを生成
       const input = document.createElement("textarea");
       // テキストエリアの内容をIDに
-      input.textContent=JSON.parse(document.getElementById("test").textContent).con;
+      input.textContent=JSON.parse(document.getElementById("copy_text").textContent).con;
       // テキストエリアにidを付与
       input.id = "copyTarget";
       // テキストエリアを指定したdivに生成（子ノードとして）
@@ -25,9 +25,7 @@ function copyToClipboard(){
     }
   }
 
-
-
-
+// モーダルウィンドウの処理（新規企業登録）
   (function(){
       'use strict';
       //[POINT]idを取得する
@@ -55,3 +53,32 @@ function copyToClipboard(){
         //このように書くことができる
         });
     })();
+
+// モーダルウィンドウの処理（予定追加）
+(function(){
+    'use strict';
+    //[POINT]idを取得する
+    var open = document.getElementById('open_event');
+    var close = document.getElementById('close_event');
+    var mask = document.getElementById('mask_event');
+    var modal = document.getElementById('modal_event');
+    //[POINT]クリックイベントを定義
+    open.addEventListener('click',function(){
+      //openではマスクとモーダル画面が表示できるようにする
+      //[POINT]クラス名を変えてCSSを当て込まないようにする
+      mask.className = '';
+      modal.className = '';
+      });
+      close.addEventListener('click',function(){
+       //closeではマスクとモーダル画面を非表示にする
+       //[POINT]クラス名を再定義し、CSSを当て込む
+        mask.className = 'hidden_event';
+         modal.className = 'hidden_event';
+      });
+         //モーダル画面外をクリックしてもモーダル画面が閉じるようにする
+      mask.addEventListener('click',function(){
+      //closeと同じなので、closeのクリックイベントを呼び出せば良い
+      close.click(); 
+      //このように書くことができる
+      });
+  })();
