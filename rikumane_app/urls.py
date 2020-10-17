0b3ca8dbc6a6a577249e751094f8fca1d352cb47
saki0311 <q231072s@mail.kyutech.jp>
2020-10-17 03:10:49 +0000

    indexのパスを変更しました

--- rikumane_app/urls.py
+++ rikumane_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name='rikumane_app'
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('detail/',views.detail,name='detail'),
 ]
