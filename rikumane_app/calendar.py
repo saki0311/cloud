7742f755f484b70c9a9d73b97e782f6bcfcdf10c
saki0311 <q231072s@mail.kyutech.jp>
2020-11-13 19:12:51 +0900

    brank

--- rikumane_app/calendar.py
+++ rikumane_app/calendar.py
@@ -1,7 +1,7 @@
 import pickle
 import os.path
 import datetime
-from googleapiclient.discovery import build
+# from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request   
 
