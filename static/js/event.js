bdf209646391d6338c412699788483e3ea672cb5
gen <sono2510@gmail.com>
2020-12-08 17:18:48 +0900

    イベント追加のモーダルウィンドウ実装

--- static/js/event.js
+++ static/js/event.js
@@ -9,12 +9,18 @@
 
 var btn = document.getElementById('open_btn');
 var modal = document.getElementById('modalArea');
-var closeBtn = document.getElementById('close');
+var closeBtn = document.getElementById('close_modal');
 
 btn.addEventListener('click', function() {
   modal.style.display = 'block';
-})
+});
 
 closeBtn.addEventListener('click', function() {
   modal.style.display = 'none';
-})
+});
+
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+      modal.style.display = 'none';
+    }
+});
