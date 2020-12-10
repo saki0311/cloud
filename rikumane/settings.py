4c251eed928b289b81dfe2737dec0d2fb12b84d0
ayaka8864 <q232013a@gmail.com>
2020-12-10 19:53:45 +0900

    イベント追加フォームデザイン

--- rikumane/settings.py
+++ rikumane/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'rikumane.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rikumane',
+        'NAME': 'database_rikumane',
         'USER': 'root',
-        'PASSWORD': 'password',
-        # 'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
+        'PASSWORD': 'root',
+        'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
         'PORT': '3306'
     }
 }
