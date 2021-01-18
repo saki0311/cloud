948caa665ffd43e5cc0265d039ae9c2b4d4e5ae3
ayaka8864 <q232013a@gmail.com>
2021-01-18 22:35:50 +0900

    何もしてない

--- rikumane/settings.py
+++ rikumane/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'rikumane.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rikumanedb',
+        'NAME': 'database_rikumane',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
